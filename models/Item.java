9e2afba359846a6b9e9242f39eb7366431d208ca
ErikMedeiros616 <130466029+ErikMedeiros616@users.noreply.github.com>
2023-06-13 21:54:06 -0300

    item

---
+++ models/Item.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bookshop.models;
+
+public class Item {
+    private String id;
+    private String title;
+    private String publisher;
+    private String year;
+
+    public Item(String id, String title, String publisher, String year) {
+        this.id = id;
+        this.title = title;
+        this.publisher = publisher;
+        this.year = year;
+    }
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+}

2c58b0d5d8acdc0a64eea59871e291623dc6215e
felipviana <fabiofelipevianasantosfacul@gmail.com>
2023-06-13 21:30:52 -0300

    Books finished

---
+++ models/Book.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bookshop.models;
+
+public class Book extends Item {
+    private String author;
+    private String ISBN;
+
+    public Book(String id, String title, String publisher, String year, String author, String ISBN) {
+        super(id, title, publisher, year);
+        this.author = author;
+        this.ISBN = ISBN;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+}
+
